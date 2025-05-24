9b3aa1daadcc56c08398e25f1d29b50d94af5dc2
igorbdaluz <igorbdaluz@hotmail.com>
2025-05-23 23:24:37 -0300

    Subindo classe da equipe. #3

--- BalonismoLIB/Balonista.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BalonismoLIB;
-
-public class Balonista
-{
-    public int Id { get; set; }
-    public string Nome { get; set; }
-}
---
+++ BalonismoLIB/Modelos/Balonista.cs
@@ -0,0 +1,9 @@
+﻿namespace BalonismoLIB.Modelos;
+
+public class Balonista
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public int IdEquipe { get; set; }
+    public Equipe Equipe { get; set; }
+}
